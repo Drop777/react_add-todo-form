aeffbd3ed69879394e7f366909485910666b3d60
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-09-22 18:38:26 +0300

    Updates

---
+++ precache-manifest.208618fb63aed7736901bcf190e20b97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175d90d4d5ab41a1cb4b0e516faf5d5c",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "1a98d136545ccfbe6d6e",
+    "url": "/react_add-todo-form/static/css/main.9602ee52.chunk.css"
+  },
+  {
+    "revision": "1d2b5461b5bb084a03fb",
+    "url": "/react_add-todo-form/static/js/2.31c96d0d.chunk.js"
+  },
+  {
+    "revision": "1a98d136545ccfbe6d6e",
+    "url": "/react_add-todo-form/static/js/main.36ac16ae.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
