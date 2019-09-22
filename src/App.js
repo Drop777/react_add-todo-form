d7451c978f252e936126bbf58c257a40742e98a7
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-09-22 18:37:17 +0300

    some fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import './App.css';
+import Newtodo from './Conponents/Newtodo/Newtodo';
+import Todolist from './Conponents/Todolist/Todolist';
 
 import users from './api/users';
+import todos from './api/todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Static list of todos</h1>
+function todosWithUsers(todosLIst, usersList) {
+  return todosLIst.map(todo => ({
+    ...todo,
+    user: usersList.find(item => item.id === todo.userId),
+  }));
+}
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todosList: todosWithUsers(todos, users),
+  };
+
+  addTodo = (todo) => {
+    this.setState(prevState => ({
+      todosList: [...prevState.todosList, todo],
+    }));
+  };
+
+  render() {
+    const { todosList } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Static list of todos</h1>
+
+        <p>
+          <span>Todos: </span>
+          {todosList.length}
+        </p>
+        <Newtodo users={users} todos={todosList} addTodo={this.addTodo} />
+        <div className="container">
+          {todosList.map(todo => <Todolist key={todo.id} todo={todo} />)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
