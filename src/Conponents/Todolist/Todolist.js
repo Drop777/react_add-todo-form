1e263adf683fc44701c7205a9ef04c1868fc996b
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-09-26 13:11:55 +0300

    'fixed'

--- src/Conponents/Todolist/Todolist.js
+++ src/Conponents/Todolist/Todolist.js
@@ -5,29 +5,27 @@ import PropTypes from 'prop-types';
 
 const Todolist = ({ todo }) => (
 
-  <>
-    <div className="container__item">
-      <h2>
-        ToDo:
-        {todo.id}
-      </h2>
-      <p>{todo.user.name}</p>
-      <p>
-        Title:
-        {todo.title}
-      </p>
-      <p>
-        UserId:
-        {todo.user.id}
-      </p>
-      <span>
-        Completed:
-        {todo.completed
-          ? ' \u2714'
-          : ' \u2716'}
-      </span>
-    </div>
-  </>
+  <div className="container__item">
+    <h2>
+      ToDo:
+      {todo.id}
+    </h2>
+    <p>{todo.user.name}</p>
+    <p>
+      Title:
+      {todo.title}
+    </p>
+    <p>
+      UserId:
+      {todo.user.id}
+    </p>
+    <span>
+      Completed:
+      {todo.completed
+        ? ' \u2714'
+        : ' \u2716'}
+    </span>
+  </div>
 );
 
 Todolist.propsTypes = {
